598e9d541d2bf24aea0cecd25b622af9974a2a20
Spencer Todd <cheesy.mac.dott@gmail.com>
2017-06-13 15:45:06 -0400

    Status bar message on execution of ws-key-func

---
+++ .i3blocks/sys-msg/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='sys_msg_int',
+    version='0.1',
+    packages=['sys_msg_int'],
+)
---
+++ .i3blocks/sys-msg/sys_msg_int/__init__.py
@@ -0,0 +1,4 @@
+"""
+An interface for writing to the i3 status bar.
+"""
+from .writer import clear, PIPE, write
