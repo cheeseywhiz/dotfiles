5086550cb41043250ed9b026e551dd5c64eec586
Spencer Todd <cheesy.mac.dott@gmail.com>
2017-07-23 11:14:17 -0400

    wal transparency

---
+++ .config/i3/cmd_dict.py
@@ -0,0 +1,56 @@
+cmd_dict = {
+    None: {
+        'cmd': ['exec'],
+        'msg': {
+            'long': None,
+            'short': None,
+            't': None,
+            'color': None,
+        },
+    },
+    1: {
+        'cmd': ['firefox', 'https://www.reddit.com/'],
+        'msg': {
+            'long': 'Opening Firefox',
+            'short': 'Firefox',
+            't': 3,
+            'color': None,
+        },
+    },
+    2: {
+        'cmd': ['subl3'],
+        'msg': {
+            'long': 'Opening Sublime',
+            'short': 'Sublime',
+            't': 3,
+            'color': None,
+        },
+    },
+    3: {
+        'cmd': ['urxvt'],
+        'msg': {
+            'long': 'Opening URxvt',
+            'short': 'URxvt',
+            't': 3,
+            'color': None,
+        },
+    },
+    4: {
+        'cmd': ['gitkraken'],
+        'msg': {
+            'long': 'Opening GitKraken',
+            'short': 'GitKraken',
+            't': 3,
+            'color': None,
+        },
+    },
+    10: {
+        'cmd': ['firefox', '--private-window', 'https://www.reddit.com/'],
+        'msg': {
+            'long': 'Opening private Firefox window',
+            'short': 'Firefox private',
+            't': 3,
+            'color': None,
+        },
+    },
+}
