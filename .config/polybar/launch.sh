1c19f7a79438d8ea6ee140465f0637b5e7fbe0f3
Spencer Todd <cheesy.mac.dott@gmail.com>
2017-07-30 12:47:09 -0400

    Furthur polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,9 +4,9 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do
-    sleep 0 &
+while pgrep -x polybar > /dev/null; do
+    sleep 0.1
 done
 
 # Launch top bar
-polybar -r -log=info top &
+polybar -r top &
