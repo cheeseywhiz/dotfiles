a7c1e1f7fd15a11fc2474d3d593cc29bad376ccc
Spencer Todd <cheesy.mac.dott@gmail.com>
2017-08-20 23:55:41 -0400

    Improvements for wallpapers sourced from reddit

--- .config/wal/launch.sh
+++ .config/wal/launch.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 # launch.sh <path>
 
-cd $HOME/.cache/wal
+WAL_CACHE=$HOME/.cache/wal
+WAL_LAST=$WAL_CACHE/wal
 
-wal -i $1 -a 93
-rm ./wal_chrome/"Cached Theme.pak" |& sudo tee /dev/null > /dev/null
-convert -scale "1366x768^" -blur "0x10" $(cat ./wal) ./wal_chrome/img/theme_ntp_background.jpg
+# give wal filename that transcends its tmp directory (for caching)
+cd $(dirname $1)
+wal -i ./$(basename $1) -a 93
+
+WAL_IMG=$(realpath $(cat $WAL_LAST))
+echo $WAL_IMG > $WAL_LAST
+
+cd $WAL_CACHE/wal_chrome
+convert $WAL_IMG -scale "1366x768^" -blur "0x10" ./img/theme_ntp_background.jpg
 $HOME/.config/wal/chrome_theme.py
