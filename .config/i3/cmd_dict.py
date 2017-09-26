684ed080aab3ede068c96ed80c9b30f40afc1034
Spencer Todd <cheesy.mac.dott@gmail.com>
2017-09-26 12:57:18 -0400

    Re-gitignore

---
+++ .config/i3/cmd_dict.py
@@ -0,0 +1,11 @@
+chromium = (
+    'chromium --new-window --load-extension=/home/cheese/.cache/wal/wal_chrome'
+)
+
+cmd_dict = {
+    1: chromium,
+    2: 'subl',
+    3: 'urxvt',
+    4: 'gitkraken',
+    10: chromium + ' --incognito https://www.reddit.com/',
+}
