7051dcbd68957ebdde6749cd2cd327da9768fd75
Spencer Todd <cheesy.mac.dott@gmail.com>
2017-10-26 00:54:13 -0400

    Use new collect flags

--- .config/i3/cmd_dict.py
+++ .config/i3/cmd_dict.py
@@ -4,7 +4,7 @@ chromium = (
 
 cmd_dict = {
     1: chromium,
-    2: 'bash -c "subl |& sudo tee /var/subllog > /dev/null"',
+    2: 'subl',
     3: 'urxvt',
     4: 'gitkraken',
     10: chromium + ' --incognito https://www.reddit.com/',
